a474e70cf6c551b6017e2836f1aa74ed95806641
Chang Zheng <chang@cmac.lan>
2020-02-11 15:55:47 -0800

    update

--- python-sandbox1/temp3.py
+++ python-sandbox1/temp3.py
@@ -25,26 +25,36 @@ def test_zip():
         for i in k:
             print(i)
 
-name = ["Manjeet", "Nikhil", "Shambhavi", "Astha"]
-roll_no = [4, 1, 3, 2]
-marks = [40, 50, 60, 70]
+def zip_fun():
+    name = ["Manjeet", "Nikhil", "Shambhavi", "Astha"]
+    roll_no = [4, 1, 3, 2]
+    marks = [40, 50, 60, 70]
 
-# using zip() to map values
-mapped = zip(name, roll_no, marks)
-for m in mapped:
-    # print('mapped =', m)
-    # if 'Astha' in m[0]:
-    #     print(m)
+    # using zip() to map values
+    mapped = zip(name, roll_no, marks)
+    for m in mapped:
+        print('mapped =', m)
+        # if 'Astha' in m[0]:
+        #     print(m)
     print(len(m),type(m))
-
+# zip_fun()
 
 # converting values to print as set
 roll_no = [43, 1, 3, 299, 3, 3, 5, 7, 99]
 roll_no2 = ['cat','zoo','Cat','k','b','z']
-print(set(roll_no2))
-
+# print(set(roll_no2))
 
 
 # printing resultant values
-print("The zipped result is : ", end="")
-print(mapped)
+# print("The zipped result is : ", end="")
+# print(mapped)
+
+tup1 = ('physics', 'chemistry', 2000, "world", 1997);
+tup2 = (1, 2, 3, 4, 5 );
+tup3 = "a", "b", "c", "d";
+tup_emp = ()
+
+tup_to_list = [i for i in tup1 if isinstance(i, str)]
+print(type(tup_to_list), tup_to_list)
+s = ''.join(' '.join(tup_to_list))
+print(type(s), s)
