393e4f8d61545719f06840fc69e6d9c6ad776a7d
Chang Zheng <chang@cmac.lan>
2020-02-20 08:03:39 -0800

    update

--- dev-design-patterns/mvc.py
+++ dev-design-patterns/mvc.py
@@ -1,7 +1,8 @@
-from factory import ShapeFactory
 
-f = ShapeFactory()
-s = f.getShape('Square')
-print(s)
-print(s.draw())
-
+@app.route('/')
+def example_page():
+    """Search the database for entries, then display them."""
+    db = get_db()
+    query = db.execute('select * from entries order by id desc')
+    entries = query.fetchall()
+    return render_template(example_page.html, entries=entries)
