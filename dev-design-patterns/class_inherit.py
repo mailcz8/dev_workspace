9aa2fe1bc08d4deeb808f90ae4ec1200474078d0
Chang Zheng <chang@cmac.lan>
2020-03-04 09:32:17 -0800

    update

--- dev-design-patterns/class_inherit.py
+++ dev-design-patterns/class_inherit.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+'''
+Course info: https://www.youtube.com/watch?v=RSl87lqOXDE
+'''
+
+
 class Employee:
     raise_amt = 1.04
     def __init__(self, fname, lname, pay):
@@ -32,7 +37,7 @@ class Developer(Employee):
 
 class Manager(Employee):
     def __init__(self, fname, lname, pay, employees=None):
-        super().__init__(self, fname, lname, pay):
+        super().__init__(self, fname, lname, pay)
         if employees is None:
             self.employees = []
         else:
@@ -43,8 +48,12 @@ class Manager(Employee):
             self.employees.append(emp)
 
     def remove_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
+        if emp  in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
 
 if __name__ == "__main__":
     emp1 = Employee("Joe", "Smith", 50000)
@@ -55,7 +64,11 @@ if __name__ == "__main__":
     dev2 = Developer("Toto", "DevTwo", 200000, 'Java')
 
     emp1_pay, emp1_after_raise = emp1.pay, emp1.apply_raise()
-    dev1_pay, dev1_after_raise, dev1_prog_lang = dev1.pay, dev1.apply_raise()
-    print('emp1:', emp1_pay, emp1_after_raise, '; raise = ', abs(emp1_pay-emp1_after_raise))
-    print('org: dev1: 150000 156000.0 ; raise =  6000.0')
-    print('dev1:', dev1_pay, dev1_after_raise, '; raise = ', abs(dev1_pay-dev1_after_raise))
+    # dev1_pay, dev1_after_raise, dev1_prog_lang = dev1.pay, dev1.apply_raise()
+    # print('emp1:', emp1_pay, emp1_after_raise, '; raise = ', abs(emp1_pay-emp1_after_raise))
+    # print('org: dev1: 150000 156000.0 ; raise =  6000.0')
+    # print('dev1:', dev1_pay, dev1_after_raise, '; raise = ', abs(dev1_pay-dev1_after_raise))
+
+    mgr1 = Manager('Sue', "Smith", 900000, [dev1, dev2])
+    print(mgr1.email)
+    mgr1.print_emps()
