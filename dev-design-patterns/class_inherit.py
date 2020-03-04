ac930f4171eecf83878d89e4c1d53aae4f74a42a
Chang Zheng <chang@cmac.lan>
2020-03-04 09:20:59 -0800

    update

--- dev-design-patterns/class_inherit.py
+++ dev-design-patterns/class_inherit.py
@@ -20,6 +20,9 @@ class Developer(Employee):
 
     def __init__(self, fname, lname, pay, prog_lang):
         super().__init__(fname, lname, pay)
+        # the following init call is ok, but not the best
+        # Employee.__init__(self, fname, lname, pay)
+        self.prog_lang = prog_lang
 
     # def apply_raise(self):
     #     raise_amt = 1.5
@@ -27,16 +30,32 @@ class Developer(Employee):
     #     # return self.pay
 
 
+class Manager(Employee):
+    def __init__(self, fname, lname, pay, employees=None):
+        super().__init__(self, fname, lname, pay):
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
 if __name__ == "__main__":
     emp1 = Employee("Joe", "Smith", 50000)
     emp2 = Employee("Black", "Pony", 80000)
     print(emp1.email, emp2.full_name(), emp2.apply_raise())
 
-    dev1 = Developer("Uno", "DevOne", 150000)
-    dev2 = Developer("Toto", "DevTwo", 200000)
+    dev1 = Developer("Uno", "DevOne", 150000, "Python")
+    dev2 = Developer("Toto", "DevTwo", 200000, 'Java')
 
     emp1_pay, emp1_after_raise = emp1.pay, emp1.apply_raise()
-    dev1_pay, dev1_after_raise = dev1.pay, dev1.apply_raise()
+    dev1_pay, dev1_after_raise, dev1_prog_lang = dev1.pay, dev1.apply_raise()
     print('emp1:', emp1_pay, emp1_after_raise, '; raise = ', abs(emp1_pay-emp1_after_raise))
     print('org: dev1: 150000 156000.0 ; raise =  6000.0')
     print('dev1:', dev1_pay, dev1_after_raise, '; raise = ', abs(dev1_pay-dev1_after_raise))
